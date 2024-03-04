d2b6cc56ba43d73d7d0af1bd2213b4a5b75c9801
Abhishek <abhikoundal1019@gmail.com>
2024-03-04 16:58:43 +0530

    added the new add,patch,post methods

--- backend/note_app_api/urls.py
+++ backend/note_app_api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import Task_List, Task_Detail
 
 urlpatterns = [
-    path('', views.todo_api_view.as_view()),
+    path('tasks/',Task_List.as_view(), name='task-list'),
+    path('add-task/',Task_List.as_view(), name='add-task'),
+    path('task-update/<int:task_id>', Task_Detail.as_view(), name='update the task')
 ]
